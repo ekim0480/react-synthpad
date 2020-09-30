9e6bc7e88b270e7f52c9d1aa16ee141b9445463b
EK <ekim0480@gmail.com>
2020-09-29 19:27:37 -0500

    scaffolds of components and layouts

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AppContextProvider } from './context'
+import Layout from './components/layout'
+
+function App() {
+  return (
+    <AppContextProvider>
+      <Layout>
+
+      </Layout>
+    </AppContextProvider>
+  )
+}
+
+export default App;
